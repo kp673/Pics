d56b98bcbee86e5eee5c3dad6667ae41301220f7
Kush <kush.patel1@optum.com>
2021-06-10 00:08:09 -0400

    Unsplash API request using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) { }
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID OdCt5quGvlKdOeochESPHqVZY0EAuLS-OabeTYLKucc'
+      }
+    });
+
+    this.setState({ images: response.data.results });
+    console.log(response.data.results);
+
+  }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+
+        Found: {this.state.images.length} images.
       </div>
     );
   }
