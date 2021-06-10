48c356a109a9ecf74e85a22f3f385af02c061c3b
Kush <kush.patel1@optum.com>
2021-06-10 00:19:37 -0400

    refactor to create custom client

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,6 @@ class SearchBar extends React.Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    console.log(this.state.term);
     this.props.onSubmit(this.state.term);
   };
 
